cee0a9f30bca646dda7860c3bfe83499b906a8df
YoonHwan.Cho <art-1st@naver.com>
2018-11-24 22:59:53 +0900

    modify env.js

--- back-end/env.js
+++ back-end/env.js
@@ -6,5 +6,7 @@ module.exports.env = () => ({
   DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_DATABASE: process.env.DB_DATABASE
+  DB_DATABASE: process.env.DB_DATABASE,
+  NCP_AI_CLIENT_ID: process.env.NCP_AI_CLIENT_ID,
+  NCP_AI_CLIENT_SECRET: process.env.NCP_AI_CLIENT_SECRET
 });
