0b7499a48dcda0205c5496d62560c99c0ab089ad
YoonHwan.Cho <art-1st@naver.com>
2018-11-25 03:37:39 +0900

    disable circleci cache, reset geocode

--- back-end/src/router/geocode/geocode.ctrl.ts
+++ back-end/src/router/geocode/geocode.ctrl.ts
@@ -2,24 +2,22 @@ import { Context } from 'koa';
 import axios from 'axios';
 
 export const geocode = async (ctx: Context) => {
-    ctx.type = 'application/json';
-    ctx.body = '[{ "test": "good?" }]';
-    // await axios.get('https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode', {
-    //     params: {
-    //         query: ctx.params.query
-    //     },
-    //     headers: {
-    //         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    //         'X-NCP-APIGW-API-KEY-ID': process.env.NCP_AI_CLIENT_ID,
-    //         'X-NCP-APIGW-API-KEY': process.env.NCP_AI_CLIENT_SECRET
-    //     }
-    // })
-    // .then(response => {
-    //     ctx.type = 'application/json';
-    //     ctx.body = JSON.stringify(response.data);
-    // })
-    // .catch(error => {
-    //     ctx.status = 500;
-    //     ctx.body = 'Error';
-    // });
+    await axios.get('https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode', {
+        params: {
+            query: ctx.params.query
+        },
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'X-NCP-APIGW-API-KEY-ID': process.env.NCP_AI_CLIENT_ID,
+            'X-NCP-APIGW-API-KEY': process.env.NCP_AI_CLIENT_SECRET
+        }
+    })
+    .then(response => {
+        ctx.type = 'application/json';
+        ctx.body = JSON.stringify(response.data);
+    })
+    .catch(error => {
+        ctx.status = 500;
+        ctx.body = 'Error';
+    });
 }
