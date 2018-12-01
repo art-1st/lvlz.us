e952515c04f1685e4a1c53160b9396d0a3c0c9d4
YoonHwan.Cho <art-1st@naver.com>
2018-12-02 02:47:10 +0900

    reposition sidebar

--- front-end/src/components/Sidebar/Sidebar.js
+++ front-end/src/components/Sidebar/Sidebar.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import { GoCalendar } from 'react-icons/go';
-import { MdHome, MdTimeline, MdSearch } from 'react-icons/md';
-import { IoIosMail, IoMdMenu } from 'react-icons/io';
+import { MdTimeline, MdSearch } from 'react-icons/md';
+import { IoIosToday, IoIosMail, IoMdMenu } from 'react-icons/io';
 
 import styles from './Sidebar.module.scss';
 
@@ -14,11 +14,18 @@ class Sidebar extends Component {
   }
 
   menus = [
+    // {
+    //   url: "/",
+    //   icon: MdHome,
+    //   title: "홈",
+    //   isActive: this.props.menu === "home",
+    //   isDisabled: true
+    // },
     {
-      url: "/",
-      icon: MdHome,
-      title: "홈",
-      isActive: this.props.menu === "Home",
+      url: "/today",
+      icon: IoIosToday,
+      title: "투데이",
+      isActive: this.props.menu === "today",
       isDisabled: true
     },
     {
@@ -43,53 +50,69 @@ class Sidebar extends Component {
     }
   ]
 
+  toggleExpand = (e) => {
+    e.preventDefault();
+
+    this.setState((prevState) => {
+      return {
+        isExpanded: !prevState.isExpanded
+      }
+    });
+  }
+
   render() {
     return (
-      <header className={`${ styles.header } ${ this.state.isExpanded ? styles['is-expanded'] : '' }`}>
-        <h1 className="title">
-          러블리즈 팬덤<br />
-          스케줄 캘린더
-        </h1>
-        <nav className={ styles.nav }>
-          <div className="hb-menu">
-            <button onClick={() => { this.setState((prevState) => { return { isExpanded: !prevState.isExpanded } }) }} title="메뉴">
-              <IoMdMenu size={ 32 } color="#555" />
-            </button>
-          </div>
-          <ul>
-            {
-              this.menus.map((value, key) => {
-                return (
-                  <li
-                    className={`nav-item${ value.isActive ? ' is-active' : '' }${ value.isDisabled ? ' is-disabled' : '' }`}
-                    key={ key }
-                  >
-                    <Link to={ value.url }>
-                      <value.icon className="item-ico" />
-                      <span className="item-title">
-                        { value.title }
-                      </span>
-                    </Link>
-                  </li>
-                );
-              })
-            }
-            <li className="nav-item">
-              <a href="mailto:admin@art1st.me?subject=[스케줄 제보] ">
-                <IoIosMail className="item-ico" />
-                <span className="item-title">스케줄 제보</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="mailto:admin@art1st.me?subject=[개선/버그 피드백] ">
-                <IoIosMail className="item-ico" />
-                <span className="item-title">개선/버그 피드백</span>
-              </a>
-            </li>
-          </ul>
-          <strong className="nav-feedback-email">Email: admin@art1st.me</strong>
-        </nav>
-      </header>
+      <>
+        <header className={`${ styles.header } ${ this.state.isExpanded ? styles['is-expanded'] : '' }`}>
+          <h1 className="title">
+            러블리즈 팬덤<br />
+            스케줄 캘린더
+          </h1>
+          <nav className={ styles.nav }>
+            <div className="hb-menu">
+              <button onClick={(e) => { this.toggleExpand(e) }} title="메뉴">
+                <IoMdMenu size={ 32 } color="#555" />
+              </button>
+            </div>
+            <ul>
+              {
+                this.menus.map((value, key) => {
+                  return (
+                    <li
+                      className={`nav-item${ value.isActive ? ' is-active' : '' }${ value.isDisabled ? ' is-disabled' : '' }`}
+                      key={ key }
+                    >
+                      <Link to={ value.url }>
+                        <value.icon className="item-ico" />
+                        <span className="item-title">
+                          { value.title }
+                        </span>
+                      </Link>
+                    </li>
+                  );
+                })
+              }
+              <li className="nav-item">
+                <a href="mailto:admin@art1st.me?subject=[스케줄 제보] ">
+                  <IoIosMail className="item-ico" />
+                  <span className="item-title">스케줄 제보</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="mailto:admin@art1st.me?subject=[개선/버그 피드백] ">
+                  <IoIosMail className="item-ico" />
+                  <span className="item-title">개선/버그 피드백</span>
+                </a>
+              </li>
+            </ul>
+            <strong className="nav-feedback-email">Email: admin@art1st.me</strong>
+          </nav>
+        </header>
+        {
+          this.state.isExpanded &&
+          <div className={ styles['sidebar-dimmer'] } onClick={(e) => { this.toggleExpand(e) }} />
+        }
+      </>
     )
   }
 }
